c744cb062482fb0413e7b3db4e0f0c23ea572608
sakibbbbbb258 <shshssm3@gmail.com>
2025-07-08 23:10:16 +0600

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || " RAHEEM-XMD-2>>>",
+SESSION_ID: process.env.SESSION_ID || "RAHEEM-XMD-2>>>Ck4gCISY#PHh9UvCPhlTKzZLIoyKNkEL--kopeFQnPfWiWmsQY4M",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
@@ -23,11 +23,11 @@ ANTI_LINK: process.env.ANTI_LINK || "false",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/xp1wdz.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/r85tqj.mp4",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || "+",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "RAHEEM-XMD-3",
+BOT_NAME: process.env.BOT_NAME || "..YoUr Sakib..",
 // add bot namw here for menu
 STICKER_NAME: process.env.STICKER_NAME || "𝗜𝗟 𝗡𝗘 𝗙𝗔𝗨 𝗣𝗔𝗦 𝗔𝗧𝗧𝗘𝗡𝗗𝗥𝗘 𝗗'𝗘̂𝗧𝗥𝗘 𝗣𝗔𝗥𝗙𝗔𝗜𝗧 𝗣𝗢𝗨𝗥 𝗖𝗢𝗠𝗠𝗘𝗡𝗖𝗘𝗥 𝗤𝗨𝗘𝗟𝗤𝗨𝗘𝗦 𝗖𝗛𝗢𝗦𝗘 𝗗𝗘 𝗕𝗜𝗘𝗡",
 // type sticker pack name 
@@ -37,13 +37,13 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "255763111390",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "8801890598817",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "RAHEEM-CM",
+OWNER_NAME: process.env.OWNER_NAME || "YoUr sakin",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴇᴠ RAHEEM-CM*",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/xp1wdz.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/r85tqj.mp4",
 // add img for alive msg
 LIVE_MSG: process.env.LIVE_MSG || "> * 𝒜𝐵𝒟𝒰𝐿𝑅𝒜𝐻𝐼𝑀 to RAHEEM-XMD-3*❄️",
 // add alive msg here 
